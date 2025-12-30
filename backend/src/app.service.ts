870252f4e015bd145b86fa4244603446f8af2c78
231705-beep <231705@students.au.edu.pk>
2025-12-30 11:15:18 +0500

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,17 +1,8 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { EmailService } from './modules/email/email.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService implements OnModuleInit {
-  constructor(private readonly emailService: EmailService) { }
-
-  async onModuleInit() {
-    await this.emailService.verify();
-
-    await this.emailService.sendMail(
-      'YOUR_EMAIL@gmail.com',
-      'SMTP WORKING',
-      'If you receive this, email is fixed.'
-    );
+export class AppService {
+  getHello(): string {
+    return 'Medical AI System API is running.';
   }
 }
